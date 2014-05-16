dcb9e9fc68c2ecdf322562b1ac1685ae6a7b2ca1
Ketil Øvrebø <ketilovre@gmail.com>
2014-05-16 19:30:01 +0200

    Moved coverage from Coveralls to CodeClimate

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-karma-coveralls');
 
     grunt.initConfig({
 
